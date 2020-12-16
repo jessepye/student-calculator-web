561aa1853114b88cb598205c1e3a80ce5dc92cad
Jesse Pye <jessepye@gmail.com>
2020-12-16 12:09:09 -0600

    created get, set, and appendCurrentScreen functions

--- src/layout/Calculator/Calculator.js
+++ src/layout/Calculator/Calculator.js
@@ -7,23 +7,51 @@ function Calculator() {
   const [equation, setEquation] = useState('');
   const [result, setResult] = useState('0');
   const approxPrompt = '(Enter an approximation)';
-  const [approximation, setApproximation] = useState(approxPrompt);
+  const [approximation, setApproximation] = useState('');
   /*
+    Mode can be one of these strings:
     EnterintEquation
     EnteringApproximation
     Finished
   */
   const [mode, setMode] = useState('EnteringEquation');
 
+  const setCurrentScreen = (s) => {
+    if(mode==='EnteringEquation') {
+      setEquation(s);
+    } else if (mode === 'EnteringApproximation') {
+      let lastChar = s[s.length-1];
+      if(s.length === 0 || (lastChar >= '0' && lastChar <= '9') || lastChar === '.') {
+        setApproximation(s);
+      }
+    } else if (mode === 'Finished') {
+      setEquation(s);
+    }
+  };
+
+  const getCurrentScreen = () => {
+    if(mode==='EnteringEquation') {
+      return equation;
+    } else if (mode === 'EnteringApproximation') {
+      return approximation;
+    } else if (mode === 'Finished') {
+      return equation;
+    }
+  };
+
+  const appendCurrentScreen = (s) => {
+    setCurrentScreen(getCurrentScreen() + s);
+  };
+
   const onButtonPress = event => {
     const pressedButton = event.target.innerHTML;
     if (pressedButton === 'C') {
-      setEquation('');
+      setCurrentScreen('');
     }
     else if ((pressedButton >= '0' && pressedButton <= '9') || pressedButton === '.') {
-      setEquation(equation + pressedButton);
+      appendCurrentScreen(pressedButton);
     }
-    else if (['+', '-', '*', '×', '/', '%'].indexOf(pressedButton) !== -1) setEquation(equation + ' ' + pressedButton + ' ');
+    else if (['+', '-', '*', '×', '/', '%'].indexOf(pressedButton) !== -1) appendCurrentScreen(' ' + pressedButton + ' ');
     else if (pressedButton === 'Enter') {
       try {
         const evalResult = evaluate(equation.replace('×', '*'));
@@ -33,8 +61,7 @@ function Calculator() {
       }
     }
     else {
-      setEquation(equation.trim());
-      setEquation(equation.substr(0, equation.length - 1));
+      setCurrentScreen(getCurrentScreen().substr(0, equation.length - 1));
     }
   }
 
